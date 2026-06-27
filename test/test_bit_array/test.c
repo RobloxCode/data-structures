ef720c8ab471ff892f6c88d5702fc6ae8ac93140
Miguel Loranca <mloranca703@gmail.com>
2026-06-26 23:20:49 -0600

    added a test

---
+++ test/test_bit_array/test.c
@@ -0,0 +1,35 @@
+#include "../../bit_array/bit_array.h"
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int fail = 0;
+
+void test_get(struct u8_BitArr *ba, int pos);
+
+int main(void) {
+    unsigned int items = 0b10010010;
+    struct u8_BitArr byte = {.items = items};
+
+    u8_bit_arr_print(&byte);
+
+    test_get(&byte, 0);
+    test_get(&byte, 4);
+    test_get(&byte, 8);
+    test_get(&byte, 7);
+
+    return fail ? EXIT_FAILURE : EXIT_SUCCESS;
+}
+
+void test_get(struct u8_BitArr *ba, int pos) {
+    int bit = 0;
+    int status = 0;
+
+    if ((status = u8_bit_arr_get(ba, pos, &bit)) != 0) {
+        fprintf(stderr, "ERROR status: %d\n", status);
+        fail = 1;
+    }
+
+    printf("item at posisio: %d is: %d\n", pos, bit);
+}
