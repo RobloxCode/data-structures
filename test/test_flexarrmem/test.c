efb8a986bb1a6a292984e8d44db0ad1ddb009227
Miguel Loranca <mloranca703@gmail.com>
2026-06-26 23:53:20 -0600

    added the pop test

--- test/test_flexarrmem/test.c
+++ test/test_flexarrmem/test.c
@@ -6,6 +6,7 @@
 int fail = 0;
 
 void test_push(FlexArr **fa, int val);
+void test_pop(FlexArr *fa);
 void test_get(FlexArr *fa, size_t i);
 void test_len(FlexArr *fa);
 
@@ -20,6 +21,12 @@ int main(void) {
     test_push(&arr, 30);
     test_push(NULL, 40);
     test_push(&arr, 50);
+    test_push(&arr, 40);
+
+    flex_arr_println(arr);
+
+    test_pop(arr);
+    test_pop(arr);
 
     flex_arr_println(arr);
 
@@ -35,6 +42,15 @@ int main(void) {
 void test_push(FlexArr **fa, int val) {
     FA_status status = FA_OK;
     if ((status = flex_arr_push(fa, val)) != FA_OK) {
+        fprintf(stderr, "ERROR status: %d\n", status);
+        fail = 1;
+        return;
+    }
+}
+
+void test_pop(FlexArr *fa) {
+    FA_status status = FA_OK;
+    if ((status = flex_arr_pop(fa)) != FA_OK) {
         fprintf(stderr, "ERROR status: %d\n", status);
         fail = 1;
         return;
