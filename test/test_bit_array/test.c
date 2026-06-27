67d715b101994d82c393141a0e4c1353da21ff39
Miguel Loranca <mloranca703@gmail.com>
2026-06-26 23:26:57 -0600

    added a test

--- test/test_bit_array/test.c
+++ test/test_bit_array/test.c
@@ -7,6 +7,7 @@
 int fail = 0;
 
 void test_get(struct u8_BitArr *ba, int pos);
+void test_set(struct u8_BitArr *ba, int pos);
 
 int main(void) {
     unsigned int items = 0b10010010;
@@ -18,6 +19,12 @@ int main(void) {
     test_get(&byte, 4);
     test_get(&byte, 8);
     test_get(&byte, 7);
+
+    test_set(&byte, 0);
+    test_set(&byte, 1);
+    test_set(&byte, 2);
+
+    u8_bit_arr_print(&byte);
 
     return fail ? EXIT_FAILURE : EXIT_SUCCESS;
 }
@@ -31,5 +38,16 @@ void test_get(struct u8_BitArr *ba, int pos) {
         fail = 1;
     }
 
-    printf("item at posisio: %d is: %d\n", pos, bit);
+    printf("item at position: %d is: %d\n", pos, bit);
+}
+
+void test_set(struct u8_BitArr *ba, int pos) {
+    int status = 0;
+
+    if ((status = u8_bit_arr_set(ba, pos)) != 0) {
+        fprintf(stderr, "ERROR status: %d\n", status);
+        fail = 1;
+    }
+
+    printf("you set the position: %d\n", pos);
 }
