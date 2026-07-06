97f42ad0f38f517a315f56df8d7c5c6db6810ad5
Miguel Loranca <mloranca703@gmail.com>
2026-07-06 10:03:54 -0600

    added documentation

--- flexible_array_member/flexible_array_member.h
+++ flexible_array_member/flexible_array_member.h
@@ -2,6 +2,9 @@
 #define FLEXIBLE_ARRAY_MEMBER_H
 #include <stddef.h>
 
+/**
+ * @brief Status codes returned by Array List operations.
+ */
 typedef enum {
     FA_OK,
     FA_ERR_WRONG_PTR,
@@ -9,13 +12,27 @@ typedef enum {
     FA_ERR_REALLOC,
 } FA_status;
 
+/**
+ * @brief Dynamic array of ints.
+ *
+ * @var cap Number of items that the buffer can hord
+ * @var len Number of items stored
+ * @var arr Buffer for items
+ */
 typedef struct {
     size_t cap;
     size_t len;
     int arr[];
 } FlexArr;
 
+/**
+ * @brief Creates a FlexArr and initializes it
+ *
+ * @param cap Initial capacity
+ * @return Pointer to a heap allocated FlexArr or NULL on failure on cap 0
+ */
 FlexArr *flex_arr_init(const size_t cap);
+
 FA_status flex_arr_deinit(FlexArr **fa);
 FA_status flex_arr_push(FlexArr **fa, int val);
 FA_status flex_arr_pop(FlexArr *fa);
