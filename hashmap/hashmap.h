30ee82e74b1807bb8d5a7f336885c62d868b50a8
Miguel Loranca <mloranca703@gmail.com>
2026-07-07 20:00:40 -0600

    added documentation

--- hashmap/hashmap.h
+++ hashmap/hashmap.h
@@ -8,13 +8,13 @@
  *
  * @var key a string literal that would be the key
  * @var val value to be stored
- * @var occupied Number that represents weather its space in memory
+ * @var occupied Number that represents wether its space in memory
  *      has already been used (this is used for collisions)
  */
 typedef struct {
-    char *key;
-    int val;
-    int _occupied;
+    char *key;     /* < heap allocated string */
+    int val;       /* < every individual value a*/
+    int _occupied; /* < wether or not a space has been used*/
 } MapItem;
 
 // capacity of the array of elements for the hash map
@@ -23,17 +23,53 @@ typedef struct {
 /**
  * @brief Hash map struct
  *
- * @var VALUES
+ * @var elements Array of MapItem
+ * @var count the current amount of elements stored
  */
 typedef struct {
     MapItem elements[ELEMENTS_CAP];
     size_t count;
 } HashMap;
 
+/**
+ * @brief adds a new element to the hash map
+ *
+ * @param hm Pointer to HashMap
+ * @param key String that will be the key
+ * @param val Value to be added
+ */
 void hash_map_insert(HashMap *hm, const char *key, int val);
+
+/**
+ * @brief Frees the keys since they are heap allocated
+ *
+ * @param hm Pointer to HashMap
+ */
 void hash_map_deinit(HashMap *hm);
+
+/**
+ * @brief Change an existing value within the hash map's elements
+ *
+ * @param hm Pointer to HashMap
+ * @param key String that will be the key
+ * @param val new number to be changed
+ */
 int hash_map_update(HashMap *hm, const char *key, int val);
+
+/**
+ * @brief Retrieve an element with a given key
+ *
+ * @param hm Pointer to HashMap
+ * @param key String that will be the key
+ * @param out Pointer to the buffer output
+ */
 int hash_map_get(HashMap *hm, const char *key, int *out);
+
+/**
+ * @brief prints to stdout the hash map elemnts with format {key: value}
+ *
+ * @param hm Pointer to HashMap
+ */
 void hash_map_println(HashMap *hm);
 
 #endif
