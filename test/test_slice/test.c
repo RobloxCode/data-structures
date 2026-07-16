7e1fdfa4b7da7e6fc717756dd4e3c006694e6f50
Miguel Loranca <mloranca703@gmail.com>
2026-07-15 23:06:53 -0600

    added the test

--- test/test_slice/test.c
+++ test/test_slice/test.c
@@ -1,5 +1,20 @@
 #include "../../slice/slice.h"
 
+#include <stddef.h>
+
+#define LEN(a) (sizeof(a) / sizeof(a[0]))
+
 int main(void) {
+    int buf[100];
+
+    for (size_t i = 0; i < LEN(buf); ++i) {
+        buf[i] = (int)i + 1;
+    }
+
+    intSlice slice;
+    slice_init(&slice, buf, LEN(buf), 0, 50);
+
+    slice_println(&slice);
+
     return 0;
 }
