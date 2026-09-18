46407decd699226b8c862ad6c04614b843fd2248
Miguel Loranca <mloranca703@gmail.com>
2026-09-17 21:13:40 -0600

    added tests

--- test/test_string/test.c
+++ test/test_string/test.c
@@ -20,5 +20,15 @@ int main(void) {
 
     ASSERT_MSG(str != NULL, "error creating String");
 
+    string_println(str);
+    string_append_cstr(str, "hello");
+    string_println(str);
+    string_append_cstr(str, " how are you");
+
+    string_append_char(str, 'e');
+    string_println(str);
+
+    printf("len: %zu\n", string_len(str));
+
     string_free(&str);
 }
